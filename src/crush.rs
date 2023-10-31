889c543b3b972e42858f363fecfaea6cfe642f3f
bastianm <bastian.mathison@outlook.com>
2023-10-31 14:38:17 +1100

    Changed license header

--- src/crush.rs
+++ src/crush.rs
@@ -1,16 +1,16 @@
 /* License:
 * This file is part of term_img.
 
-* Foobar is free software:
+* term_img is free software:
 * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
 * either version 3 of the License, or (at your option) any later version.
 
-* Foobar is distributed in the hope that it will be useful,
+* term_img is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY;
 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 * See the GNU General Public License for more details.
 
-*  You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+*  You should have received a copy of the GNU General Public License along with term_img. If not, see <https://www.gnu.org/licenses/>.
 */
 
 use image::{DynamicImage, Rgba, Rgba32FImage};
